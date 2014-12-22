e7aa5a534eb00e40612da68d734cf697e9e31017
Brian Parks <bparks@synapsesoftware.com>
2014-12-21 18:04:56 -0700

    Better sample app with more direction

--- default/views/_layout.php
+++ default/views/_layout.php
@@ -2,16 +2,16 @@
 <html>
 <head>
 <title>My First Pails App</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css" />
+<link href="/css/custom.css" rel="stylesheet" type="text/css" />
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div>
+<div class="container">
 	<h1>My First Pails App</h1>
-	<p>Welcome to Pails! More documentation can be found at
-	<a href="https://github.com/bparks/pails">the project's github repository</a>
-	or at <a href="http://bparks.github.io/">Brian's blog on the subject</a>.
-	If you like pails, <a href="mailto:bparks@synapsesoftware.com">tell us
-	about it</a>!
-	</p>
+	<hr />
 	<?php $this->render(); ?>
 </div>
 </body>
