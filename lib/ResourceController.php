be864e710470747453bfdbaa14a9b0645fd326e6
Brian Parks <bparks@brianparks.me>
2025-08-20 15:11:53 -0700

    Fix routing to 'new' paths

--- lib/ResourceController.php
+++ lib/ResourceController.php
@@ -7,36 +7,40 @@ class ResourceController extends Controller
 	protected $default_action = null;
 	private $last_called = '';
 
-	function __call($name, $args)
+    function __call($name, $args)
     {
-		// We can't write a "new" function, but it's a convenient pathname
+        if ($name == $this->last_called) {
+            return $this->notFound();
+        }
+
+        $this->last_called = $name;
+
+        // We can't write a "new" function, but it's a convenient pathname
         if ($name == "new") {
-            $name = "_new";
-		}
+            // Process the given action
+            $this->do_before_actions($name);
+            $ret = $this->_new($args);
+            $this->do_after_actions($name);
+            return $ret;
+        } else {
+            // Process the given action
+            $opts = count($args) > 0 && count($args[0]) > 0 ? $args[0] : array('show');
+            $action = array_shift($opts);
+            $action = trim($action) == '' ? 'show' : $action;
 
-		if ($name == $this->last_called) {
-			return $this->notFound();
-		}
+            $method_name = null;
+            if (isset($this->actions[$action])) {
+                $method_name = $this->actions[$action];
+            } elseif ($this->default_action != null) {
+                $method_name = $this->actions[$this->default_action];
+            } else {
+                return $this->notFound();
+            }
 
-		$this->last_called = $name;
-
-		// Process the given action
-        $opts = count($args) > 0 && count($args[0]) > 0 ? $args[0] : array('show');
-        $action = array_shift($opts);
-		$action = trim($action) == '' ? 'show' : $action;
-
-		$method_name = null;
-		if (isset($this->actions[$action])) {
-			$method_name = $this->actions[$action];
-		} elseif ($this->default_action != null) {
-			$method_name = $this->actions[$this->default_action];
-		} else {
-			return $this->notFound();
-		}
-
-		$this->do_before_actions($action);
-		$ret = $this->$method_name($name, $opts);
-		$this->do_after_actions($action);
-		return $ret;
+            $this->do_before_actions($action);
+            $ret = $this->$method_name($name, $opts);
+            $this->do_after_actions($action);
+            return $ret;
+        }
     }
 }
